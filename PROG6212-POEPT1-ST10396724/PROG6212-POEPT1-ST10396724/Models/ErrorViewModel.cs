624afdbccccedbdbe2b625d33e2eede6612220c4
Michaelamm84 <ST10396724@vcconnect.edu.za>
2024-09-09 10:19:44 +0100

    creation

---
+++ PROG6212-POEPT1-ST10396724/PROG6212-POEPT1-ST10396724/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PROG6212_POEPT1_ST10396724.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
